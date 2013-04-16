c07a8737d329639e4df09188ddea51211fd34b17
Fabio Previtali <fabioprevitali@tiscali.it>
2013-04-16 12:45:26 +0200

    First commit.

---
+++ src/WaypointNavigation.cpp
@@ -0,0 +1,427 @@
+#include "WaypointNavigation.h"
+#include "WaypointNavigationNames.h"
+#include "Utils/Utils.h"
+#include <ros/ros.h>
+#include <tf/LinearMath/Matrix3x3.h>
+#include <tf/transform_broadcaster.h>
+#include <LinearMath/btQuaternion.h>
+#include <fstream>
+
+using namespace std;
+using namespace ros;
+using geometry_msgs::Pose;
+using geometry_msgs::Quaternion;
+using move_base_msgs::MoveBaseActionResult;
+using move_base_msgs::MoveBaseGoal;
+using nav_msgs::Odometry;
+using std_msgs::String;
+
+namespace SSI
+{
+	WaypointNavigation::WaypointNavigation(const string& serverActionName) : actionClient(serverActionName,true), nodeHandle("~"), nextPoint(0),
+																			 pathFilename(""), executingPath(false), isCyclic(false)
+	{
+		nodeHandle.getParam("agentId",agentId);
+		nodeHandle.getParam(PARAMS_FILE_NAME_SERVER,pathFilename);
+		
+		subscriberCommandPath = nodeHandle.subscribe<String>(SUBSCRIBER_POINTS_LIST_STRING,1,boost::bind(&SSI::WaypointNavigation::commandPathCallback,
+																										 this,_1));
+		
+		subscriberCommandControl = nodeHandle.subscribe<String>(SUBSCRIBER_COMMAND_LOAD,1,boost::bind(&SSI::WaypointNavigation::commandLoadCallback,
+																									  this,_1));
+		
+		subscriberGoalDone = nodeHandle.subscribe<>(SUBSCRIBER_GOAL_DONE,1024,&SSI::WaypointNavigation::goalDoneCallback,this);
+		subscriberRobotPose = nodeHandle.subscribe(SUBSCRIBER_ROBOT_POSE,1024,&SSI::WaypointNavigation::updateRobotPose,this);
+		
+		publisherStringFeedback = nodeHandle.advertise<String>(PUBLISHER_END_PATH,1);
+		publisherCoordinationFeedback = nodeHandle.advertise<String>(PUBLISHER_FEEDBACK_MOTION,1);
+		
+		Utils::println(string("File to read: ") + pathFilename + string("."),Utils::Yellow);
+	}
+	
+	WaypointNavigation::~WaypointNavigation() {;}
+	
+	pair<string,WaypointNavigation::Waypoints> WaypointNavigation::emptyWaypointList()
+	{
+		return make_pair("stop",Waypoints());
+	}
+	
+	void WaypointNavigation::commandLoadCallback(const String::ConstPtr& message)
+	{
+		string command = message->data;
+		
+		transform(command.begin(),command.end(),command.begin(),::tolower);
+		
+		if (command == "reloadfile") loadFilePath();
+	}
+	
+	void WaypointNavigation::commandPathCallback(const String::ConstPtr& message)
+	{
+		Utils::print("I received this path: ",Utils::White);
+		Utils::println(message->data,Utils::Cyan);
+		
+		/// Checking if the path already exists. If so, I load it otherwise I check if it is valid and then I load it.
+		stringstream pathStream(stringstream::in | stringstream::out);
+		string pathName;
+		bool isPathLoaded;
+		
+		pathStream << message->data;
+		
+		if (!pathStream.good())
+		{
+			Utils::println("Wrong message sent...",Utils::Red);
+			
+			return;
+		}
+		
+		/// Resetting current path.
+		posesQueue.clear();
+		nextPoint = posesQueue.end();
+		
+		pathStream >> pathName;
+		
+		transform(pathName.begin(),pathName.end(),pathName.begin(),::tolower);
+		
+		MapPosesQueue::iterator it = posesQueueMap.find(pathName);
+		
+		if (it == posesQueueMap.end()) isPathLoaded = initCustomPath(pathStream);
+		else isPathLoaded = initStandardPath(it);
+		
+		if (isPathLoaded)
+		{
+			Utils::println("New path loaded correctly.\n",Utils::Green);
+			
+			/// Cancelling old goals.
+			actionClient.cancelAllGoals();
+			goToNextGoal();
+			
+			executingPath = true;
+		}
+		else Utils::println("Failed in loading new path file.\n",Utils::Red);
+	}
+	
+	void WaypointNavigation::goalDoneCallback(const MoveBaseActionResult::ConstPtr&)
+	{
+		string state;
+		
+		state = actionClient.getState().toString();
+		
+		Utils::print("Client response -> ",Utils::White);
+		Utils::println(state,Utils::Cyan);
+		
+		if (state == "SUCCEEDED")
+		{
+			String feedback;
+			stringstream s;
+			
+			s << "Agent " << agentId << ((posesQueue.size() == 1) ? " doneChasing" : "donePatroling");
+			
+			feedback.data = s.str();
+			publisherCoordinationFeedback.publish(feedback);
+			
+			goToNextGoal();
+		}
+		else
+		{
+			if ((state == "PREEMPTED") && (posesQueue.size() > 0))
+			{
+				Utils::println("I gotta send the point again.",Utils::Yellow);
+				
+				/// I send the point again.
+				--nextPoint;
+				goToNextGoal();
+			}
+		}
+	}
+	
+	void WaypointNavigation::goToNextGoal()
+	{
+		MoveBaseGoal goal;
+		
+		if ((nextPoint == posesQueue.end()) && isCyclic) nextPoint = posesQueue.begin();
+		
+		/// Checking if the path is finished.
+		if (nextPoint != posesQueue.end())
+		{
+			ostringstream s;
+			
+			s << "I'm executing step: (" << nextPoint->position.x << "," << nextPoint->position.y << ").";
+			
+			Utils::println(s.str(),Utils::White);
+			
+			/// Each launch file must contains a remap of topic /map for each robot in the simulator because the map_server node ain't launched in a
+			/// particular namespace.
+			/// 
+			///	Example:
+			///		- robot_1.launch:
+			///			< group ns="robot_1">
+			///				< remap from="/robot_1/map" to="/map" />
+			///				...
+			///			</ group>
+			/// 
+			///		- robot_2.launch:
+			///			< group ns="robot_2">
+			///				< remap from="/robot_2/map" to="/map" />
+			///				...
+			///			</ group>
+			
+			stringstream temp;
+			
+			temp << "/robot_" << agentId << "/map";
+			
+			goal.target_pose.header.frame_id = temp.str();
+			goal.target_pose.header.stamp = Time::now();
+			
+			/// rand is used to avoid nasty behaviours...
+			goal.target_pose.pose.position.x = nextPoint->position.x + ((rand() % 2) * 0.1);
+			goal.target_pose.pose.position.y = nextPoint->position.y;
+			goal.target_pose.pose.orientation.z = nextPoint->orientation.z;
+			goal.target_pose.pose.orientation.w = nextPoint->orientation.w;
+			
+			++nextPoint;
+			actionClient.sendGoal(goal);
+		}
+		else
+		{
+			String message;
+			
+			/// Here the navigation phase is done. There are 2 cases:
+			///		- the queue is empty so I received a stop command.
+			///		- the queue is not empty but I completed the path. In this case the navigation phase is indeed done.
+			if (posesQueue.size() > 0) message.data = PATH_DONE;
+			else message.data = PATH_STOP;
+			
+			executingPath = false;
+			
+			publisherStringFeedback.publish(message);
+			actionClient.cancelAllGoals();
+		}
+	}
+	
+	bool WaypointNavigation::init()
+	{
+		Utils::println("Waiting that move_base's comes up...",Utils::White);
+		
+		if (!actionClient.waitForServer())
+		{
+			Utils::println("Action server is not responding...",Utils::Red);
+			
+			return false;
+		}
+		
+		Utils::println("move_base started!",Utils::Magenta);
+		
+		/// A stop command is inserted in the queue.
+		posesQueueMap.insert(emptyWaypointList());
+		
+		/// Reading configuration file.
+		bool loadFile = loadFilePath();
+		
+		if (loadFile)
+		{
+			Utils::println("\n********************************************************",Utils::Blue);
+			Utils::println("\tWaypointNavigation initialization done",Utils::Green);
+			Utils::println("********************************************************\n",Utils::Blue);
+		}
+		else Utils::println("Unable to read waypoints' list file.",Utils::Yellow);
+		
+		return loadFile;
+	}
+	
+	bool  WaypointNavigation::initCustomPath(stringstream& pathStream)
+	{
+		string angle;
+		double x, y, theta;
+		
+		Utils::print(string("Reading a custom path: ") + pathStream.str() + string(" and it is "),Utils::White);
+		
+		pathStream >> isCyclic;
+		
+		if (isCyclic) Utils::println("cyclic.",Utils::White);
+		else Utils::println("non-cyclic.",Utils::White);
+		
+		if (pathStream.eof()) return false;
+		
+		while (pathStream.good())
+		{
+			pathStream >> x >> y >> angle;
+			
+			if (angle == "inf") theta = 2 * M_PI;
+			else theta = Utils::deg2rad(atof(angle.c_str()));
+			
+			Pose p;
+			
+			p.position.x = x;
+			p.position.y = y;
+			p.position.z = 0.0;
+			p.orientation.z = sin(theta / 2);
+			p.orientation.w = cos(theta / 2);
+			
+			ostringstream s;
+			
+			s << "Adding to custom path point: (" << p.position.x << "," << p.position.y << "," << p.orientation.z << "," << p.orientation.w << ").";
+			
+			Utils::println(s.str(),Utils::White);
+			
+			posesQueue.push_back(p);
+		}
+		
+		nextPoint = posesQueue.begin();
+		
+		return true;
+	}
+	
+	bool WaypointNavigation::initStandardPath(MapPosesQueue::iterator it)
+	{
+		isCyclic = it->second.isCyclic;
+		
+		posesQueue = it->second.points;
+		nextPoint = posesQueue.begin();
+		
+		ostringstream s;
+		
+		s << "I'm gonna execute a new " << ((isCyclic) ? "cyclic" : "non-cyclic") << " path with " << posesQueue.size() << " points.";
+		
+		Utils::println(s.str(),Utils::White);
+		
+		return true;
+	}
+	
+	bool WaypointNavigation::loadFilePath()
+	{
+		ifstream file;
+		string line, path;
+		
+		/// Opening file.
+		file.open(pathFilename.c_str());
+		
+		if (!file.is_open()) return false;
+		
+		/// Clearing old points' list.
+		posesQueueMap.clear();
+		
+		while (getline(file,line))
+		{
+			istringstream iss(line);
+			
+			iss >> path;
+			
+			/// Checking if the path has been already inserted in queue.
+			MapPosesQueue::iterator it = posesQueueMap.find(path);
+			
+			/// The path is new.
+			if (it == posesQueueMap.end())
+			{
+				/// Inserting new points' list.
+				Waypoints newWaypoints;
+				string angle;
+				double x, y, theta;
+				
+				iss >> newWaypoints.isCyclic;
+				
+				if (newWaypoints.isCyclic) Utils::println("Found a cyclic path.",Utils::Blue);
+				else Utils::println("Found a non cyclic path.",Utils::Blue);
+				
+				while (iss.good())
+				{
+					iss >> x >> y >> angle;
+					
+					if (angle == "inf") theta = 2 * M_PI;
+					else theta = Utils::deg2rad(atof(angle.c_str()));
+					
+					Pose p;
+					
+					p.position.x = x;
+					p.position.y = y;
+					p.position.z = 0.0;
+					p.orientation = tf::createQuaternionMsgFromYaw(theta);
+					
+					newWaypoints.points.push_back(p);
+					
+					stringstream s;
+					
+					s << "Adding to " << path << " point: (" << p.position.x << "," << p.position.y << "," << p.orientation.z << "," << p.orientation.w
+					  << ").";
+					
+					Utils::println(s.str(),Utils::White);
+				}
+				
+				transform(path.begin(),path.end(),path.begin(),::tolower);
+				
+				/// Inserting the new path in the queue.
+				posesQueueMap.insert(make_pair<string,Waypoints>(path,newWaypoints));
+			}
+		}
+		
+		Utils::println("File reading phase done.",Utils::Green);
+		
+		return true;
+	}
+	
+	void WaypointNavigation::updateRobotPose(const Odometry::ConstPtr& message)
+	{
+		static int64_t initialStackTime;
+		static bool firstTime = true;
+		double roll, pitch, yaw;
+		
+		const Quaternion& q = message->pose.pose.orientation;
+		
+		tf::Matrix3x3(btQuaternion(q.x,q.y,q.z,q.w)).getRPY(roll,pitch,yaw);
+		
+		robotPoseX = -message->pose.pose.position.y;
+		robotPoseY = message->pose.pose.position.x;
+		robotPoseTheta = (M_PI / 2) + yaw;
+		
+		/// Recovery procedure when the robot's stacking for some unknown reason...
+		if (executingPath)
+		{
+			if ((fabs(robotPoseX - oldRobotPoseX) < 0.05) && (fabs(robotPoseY - oldRobotPoseY) < 0.05) && (fabs(robotPoseTheta - oldRobotPoseTheta) < 0.05))
+			{
+				if (firstTime)
+				{
+					initialStackTime = Time::now().toNSec();
+					firstTime = false;
+				}
+				else
+				{
+					if ((Time::now().toNSec() - initialStackTime) > 2000000000)
+					{
+						Utils::println("Robot's stacking... I gotta send the point again.",Utils::Yellow);
+						
+						/// I send the point again.
+						--nextPoint;
+						goToNextGoal();
+						
+						firstTime = true;
+					}
+				}
+			}
+			else
+			{
+				oldRobotPoseX = robotPoseX;
+				oldRobotPoseY = robotPoseY;
+				oldRobotPoseTheta = robotPoseTheta;
+				
+				firstTime = true;
+			}
+		}
+	}
+}
+
+int main (int argc, char** argv)
+{
+	ros::init(argc,argv,"WaypointNavigation");
+	
+	SSI::WaypointNavigation waypointNavigation(SSI::ACTION_SERVER_NAME);
+	
+	if (!waypointNavigation.init())
+	{
+		SSI::Utils::println("Something went wrong during the initialization phase... Exiting!",SSI::Utils::Red);
+		
+		return -1;
+	}
+	
+	ros::spin();
+	
+	return 0;
+}
---
+++ src/WaypointNavigation.h
@@ -0,0 +1,195 @@
+#ifndef WAYPOINT_NAVIGATION_H
+#define WAYPOINT_NAVIGATION_H
+
+#include <actionlib/client/simple_action_client.h>
+#include <geometry_msgs/Pose.h>
+#include <move_base_msgs/MoveBaseAction.h>
+#include <ros/node_handle.h>
+#include <nav_msgs/Odometry.h>
+#include <std_msgs/String.h>
+#include <list>
+
+namespace SSI
+{
+	class WaypointNavigation
+	{
+		protected:
+			typedef std::list<geometry_msgs::Pose> PosesQueue;
+			
+			/**
+			 *	Represents the Waypoint structure.
+			 */
+			struct Waypoints
+			{
+				PosesQueue points;
+				bool isCyclic;
+				
+				Waypoints() : isCyclic(false) {;}
+				
+				Waypoints(const Waypoints& waypoints)
+				{
+					this->isCyclic = waypoints.isCyclic;
+					this->points = waypoints.points;
+				}
+			};
+			
+			typedef std::map<std::string,Waypoints> MapPosesQueue;
+			
+			/**
+			 *	Represents the object used for communicating with the localizer node.
+			 */
+			actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> actionClient;
+			
+			/**
+			 *	Handle to a ros node.
+			 */
+			ros::NodeHandle nodeHandle;
+			
+			/**
+			 *	Subscriber associated to topic SUBSCRIBER_POINTS_LIST_STRING.
+			 */
+			ros::Subscriber subscriberCommandPath;
+			
+			/**
+			 *	Subscriber associated to topic SUBSCRIBER_COMMAND_LOAD.
+			 */
+			ros::Subscriber subscriberCommandControl;
+			
+			/**
+			 *	Subscriber associated to topic SUBSCRIBER_GOAL_DONE.
+			 */
+			ros::Subscriber subscriberGoalDone;
+			
+			/**
+			 *	Subscriber associated to topic SUBSCRIBER_ROBOT_POSE.
+			 */
+			ros::Subscriber subscriberRobotPose;
+			
+			/**
+			 *	Publisher for sending responses to whom subscribed PUBLISHER_END_PATH.
+			 */
+			ros::Publisher publisherStringFeedback;
+			
+			/**
+			 *	Publisher for sending feedback to whom subscribed PUBLISHER_FEEDBACK_MOTION.
+			 */
+			ros::Publisher publisherCoordinationFeedback;
+			
+			/**
+			 *	Represents the map of points' list.
+			 */
+			MapPosesQueue posesQueueMap;
+			
+			/**
+			 *	Represents the points' list.
+			 */
+			PosesQueue posesQueue;
+			
+			/**
+			 *	Represents the next point in the list.
+			 */
+			PosesQueue::iterator nextPoint;
+			
+			/**
+			 *	Represents the name of the file containing the points' list.
+			 */
+			std::string pathFilename;
+			
+			/**
+			 *	Represents the robot pose.
+			 */
+			double robotPoseX, robotPoseY, robotPoseTheta;
+			
+			/**
+			 *	Represents the old robot pose.
+			 */
+			double oldRobotPoseX, oldRobotPoseY, oldRobotPoseTheta;
+			
+			/**
+			 *	Represents the agent id.
+			 */
+			int agentId;
+			
+			/**
+			 *	Am I executing a given path?
+			 */
+			bool executingPath;
+			
+			/**
+			 *	Is it a cyclic path?
+			 */
+			bool isCyclic;
+			
+			/**
+			 *	@return returns an empty waypoints list.
+			 */
+			static std::pair<std::string,Waypoints> emptyWaypointList();
+			
+			/**
+			 *	In order to send a new goal to amcl node.
+			 */
+			void goToNextGoal();
+			
+			/**
+			 *	Make ready for execution a new path received.
+			 *	@return return true if everything is gone well, otherwise false.
+			 */
+			bool initCustomPath(std::stringstream& pathStream);
+			
+			/**
+			 *	Make ready for execution a standard path.
+			 *	@return return true if everything is gone well, otherwise false.
+			 */
+			bool initStandardPath(MapPosesQueue::iterator it);
+			
+			/**
+			 *	The path filename is read from a topic.
+			 *	@return returns true if the file has been read correctly, otherwise false.
+			 */
+			bool loadFilePath();
+			
+		public:
+			/**
+			 *	Class constructor. Here, every ROS structure is inizialized.
+			 *	@param serverActionName the name of the server action. Called in the function main.
+			 */
+			WaypointNavigation(const std::string& serverActionName);
+			
+			/**
+			 *	Class destructor. Here, every structure is destroyed.
+			 */
+			virtual ~WaypointNavigation();
+			
+			/**
+			 *	Callback associated to topic SUBSCRIBER_COMMAND_LOAD.
+			 *	@param message represents the message read from the topic for reading a new file and then for sending a message "reloadFile".
+			 */
+			void commandLoadCallback(const std_msgs::String::ConstPtr& message);
+			
+			/**
+			 *	Callback associated to topic SUBSCRIBER_POINTS_LIST_STRING.
+			 *	@param message represents the message read from the topic.
+			 */
+			void commandPathCallback(const std_msgs::String::ConstPtr& message);
+			
+			/**
+			 *	Callback associated to goal topic.
+			 *	@param message represents the message read from the topic.
+			 */
+			void goalDoneCallback(const move_base_msgs::MoveBaseActionResult::ConstPtr& message);
+			
+			/**
+			 *	Initialization of every structure used.
+			 *	@return returns true if the initialization is gone well, otherwise false.
+			 */
+			bool init();
+			
+			/**
+			 *	Callback associated to robot pose topic.
+			 *	@param message represents the message read from the topic.
+			 */
+			void updateRobotPose(const nav_msgs::Odometry::ConstPtr& message);
+	};
+}
+
+#endif
